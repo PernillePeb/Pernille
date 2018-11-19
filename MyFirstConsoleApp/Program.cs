9e47ff41570f90f03fc0f7a4ab9af373b98f7f69
diversemail633 <diversemail633@gmail.com>
2018-11-19 12:36:33 +0100

    add for loop

--- MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/Program.cs
@@ -9,7 +9,18 @@ namespace MyFirstConsoleApp
     class Program
     {
         static void Main(string[] args)
-        {
+         { Console.WriteLine("Hello World");
+            Console.WriteLine();
+            Console.WriteLine("Args :" + args.Length);
+
+
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Indhold :" + args[i]);
+            }
+         
+            Console.ReadKey();
         }
     }
 }
